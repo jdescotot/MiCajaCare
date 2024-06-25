c2d023272671e06c7e7fa850bcb5407c36267219
Melek Sibrian <38340866+MelekSibrian@users.noreply.github.com>
2024-06-25 09:39:09 -0600

    agregando useEffects a StockPanel

--- screens/admin/StockPanel.tsx
+++ screens/admin/StockPanel.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import { View, Text, Button, TextInput, Modal, Dimensions  } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Slider from '@react-native-community/slider';
